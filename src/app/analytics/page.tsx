43382bde07b529589db8fa15ad3a6052de660fdb
Muhammad Faiza Fachra Azizi <fachrae@gmail.com>
2026-06-17 14:23:47 +0700

    add folder structure and pages

---
+++ src/app/analytics/page.tsx
@@ -0,0 +1,8 @@
+export default function analytics() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>Hello World!</h1>
+      <p>Ini adalah halaman Analytics.</p>
+    </div>
+  );
+}
